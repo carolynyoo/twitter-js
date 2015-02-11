b7004a05653578754759ee5948b46de6c7b4dc34
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-02-10 20:25:52 -0500

    Set up Express and get request

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+
+app.listen(3000);
+
+app.get('/', function (req, res) {
+  res.send('Hello world!');
+})
+
