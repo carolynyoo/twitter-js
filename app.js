611099f2e57eaf41c7b589eea371eec6ad22b814
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-02-10 21:07:26 -0500

    Add swig templating

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var logger = require('morgan');
+var swig = require('swig');
 
 var app = express();
 
 app.use(logger('dev'));
-
 app.listen(3000);
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + './views');
+
+// change back for production view
+swig.setDefaults({cache:false});
+
+var people = [{name: 'Sean'}, {name: 'Carolyn'}, {name: 'Zeke'}];
 
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.render('index', {title: 'Hall of Fame', people: people});
 })
 
-app.get('/news', function (req, res) {
-  res.send('This is news and improved!!!');
-})
